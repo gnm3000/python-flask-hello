596870899c8e141c019c58f61978894e2f97bc5e
German N Montenegro <gnm3000@gmail.com>
2022-06-13 21:25:45 -0300

    OK

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -3,4 +3,4 @@ from app import app
 def test_ok2():
     with app.test_client() as test_client:
         response = test_client.get("/")
-        assert response.status_code==204
+        assert response.status_code==200
