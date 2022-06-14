2b69cbae64d3f4a447856c1b90fbc323f11edabd
German N Montenegro <gnm3000@gmail.com>
2022-06-13 21:22:02 -0300

    OK

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.7
-RUN python -m pip install flask
-
-
-EXPOSE 8080
-COPY /app /app
-WORKDIR /app
-CMD ["python", "app.py"]
---
+++ tests/integration/test_app.py
@@ -0,0 +1,6 @@
+from app import app
+
+def test_ok2():
+    with app.test_client() as test_client:
+        response = test_client.get("/")
+        assert response.status_code==204
